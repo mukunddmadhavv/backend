a4514bbbe76af99264b04f4e28d402a067490ed5
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-29 11:39:13 +0530

    Update and rename Customer.js to BusinessOwner.js

---
+++ models/BusinessOwner.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const businessOwnerSchema = new mongoose.Schema({
+  businessName: { type: String, required: true },
+  mobile: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('BusinessOwner', businessOwnerSchema);
