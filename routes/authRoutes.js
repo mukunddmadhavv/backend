e49f391afca089f62791186ccd7f527c23b79ad0
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-30 16:30:51 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -43,7 +43,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid password' });
     }
 
-    res.status(200).json({ message: 'Login successful', owner: user });
+const { password, ...ownerData } = user.toObject(); // remove hashed password
+res.status(200).json({ message: 'Login successful', owner: ownerData });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Login failed', error: err });
