5e4ff054f8b61925afc28778146de53938e7c09a
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-08-03 16:54:24 +0530

    Update BusinessOwner.js

--- models/BusinessOwner.js
+++ models/BusinessOwner.js
@@ -4,6 +4,7 @@ const businessOwnerSchema = new mongoose.Schema({
   businessName: { type: String, required: true },
   mobile: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+dateJoined: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('BusinessOwner', businessOwnerSchema);
